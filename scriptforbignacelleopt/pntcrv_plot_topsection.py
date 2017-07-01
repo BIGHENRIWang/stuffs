0f3b1192ede05df6d5d29e47a94ec16095463f49
BIGHENRIWang <sywang14@fudan.edu.cn>
2017-07-01 20:15:21 +0800

    three real coordinates are done, wait to be applied to 3d nacelle top seciton

--- scriptforbignacelleopt/pntcrv_plot_topsection.py
+++ scriptforbignacelleopt/pntcrv_plot_topsection.py
@@ -1,47 +1,32 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+__author__ = "BIGHENRIWang"
+import logging
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-#
-#labels='frogs','hogs','dogs','logs'
-#sizes=15,20,45,10
-#colors='yellowgreen','gold','lightskyblue','lightcoral'
-#explode=0,0.1,0,0
-#plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
-#plt.axis('equal')
-#plt.show()
-#
-#
-#x = np.linspace(0, 10, 1000)
-#y = np.sin(x)
-#z = np.cos(x**2)
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='foilontoNacsection.log',
+                    filemode='w')
+logger = logging.getLogger(__name__)
 
 
 
-dat = pd.read_csv("boe.csv", sep=",")
+logger.info('assign the original good to-be-sub airfoil data, prepare it with columns separated by nothing but comma')
+dat = pd.read_csv("syj.csv", sep=",")
 
 line_x = dat['x']
 line_y = dat['y']
 line_z = dat['z']
 
-# plt.figure(figsize=(9,5))
-# plt.plot(line_x, line_y, label="$boe crv$", color="gold", linewidth=2.5)
-# plt.xlabel("xposition")
-# plt.ylabel("yposition")
-# plt.title("Original boe curv")
-# plt.legend()
-# plt.show()
 
 #adaption to local section...
 
 #exact local length
-K = 5.6
+K = 5.524
 
 #local A point coordination
 Ax = 1.904129
@@ -53,27 +38,20 @@ Bx = 7.366
 By = 1.439885
 Bz = -0.03332581
 
-k_line_x = K * line_x 
+#local C point coordination
+Cx = 1.841189
+Cy = 1.535586
+Cz = -0.03554083
+
+
+k_line_x = K * line_x
 k_line_y = K * line_y
 k_line_z = K * line_z
-
-print('the minimum index')
-print(k_line_x.idxmin())
-print(k_line_x.iloc[k_line_x.idxmin()])
-print(k_line_y.iloc[k_line_y.idxmin()])
-print(k_line_z.iloc[k_line_z.idxmin()])
-
-leftest_x = k_line_x.iloc[k_line_x.idxmin()]
-leftest_y = k_line_y.iloc[k_line_x.idxmin()]
-leftest_z = k_line_z.iloc[k_line_x.idxmin()]
-
-
 
 
 plt.figure(figsize=(9,5))
 ax = plt.gca()
 # ax.set_aspect('equal')
-
 
 #plot A point
 plt.scatter(Ax,Ay,label="A point", color="red", marker="p")
@@ -81,8 +59,11 @@ plt.scatter(Ax,Ay,label="A point", color="red", marker="p")
 #plot B point
 plt.scatter(Bx,By,label="B point", color="blue", marker="p")
 
-#move to the B point
+#plot C point
+plt.scatter(Cx,Cy,label="C point", color="black", marker="p")
 
+
+logger.info("move to the B point")
 translation_x = Bx - k_line_x.iloc[0]
 translation_y = By - k_line_y.iloc[0]
 
@@ -92,11 +73,6 @@ k_line_y = k_line_y + translation_y
 
 original_k_line_x = k_line_x
 original_k_line_y = k_line_y
-
-
-
-
-
 
 #here you input the desired angle changement
 pnt_num = k_line_x.size
@@ -115,15 +91,19 @@ shortest_dist_x = 0
 shortest_dist_y = 0
 shortest_dist_i =10
 
-for i in range(200,250,1):
-
+for i in range(-1100,-1):
+    i = i / 1000
     # calculate intial angle phi for each position
     delta_y = k_line_y - By
     delta_x = k_line_x - Bx
     delta_dist = np.sqrt(delta_x * delta_x + delta_y * delta_y)
     phi = np.arctan(delta_y / delta_x)
 
-    theta = math.pi/i + math.pi
+    if not i == 0.0:
+        theta = math.pi/i + math.pi
+    else:
+        theta = math.pi
+
 
     phi_new = phi - theta
     new_k_line_x = Bx + abs(delta_x) * np.cos(phi_new)
@@ -138,15 +118,11 @@ for i in range(200,250,1):
             shortest_dist_x = new_k_line_x[index]
             shortest_dist_y = new_k_line_y[index]
             shortest_dist_i = i
-    else:
-        print('maybe there is something wrong in this theta angle!')
 
-print("shortest distance, etc.:")
-print(shortest_dist)
-print(shortest_dist_x)
-print(shortest_dist_y)
-print(shortest_dist_i)
-print(shortest_dist_index)
+logger.info("shortest distance:{}".format(shortest_dist) )
+logger.info("shortest distance: the nearest point to B (x,y):{},{}".format(shortest_dist_x, shortest_dist_y) )
+logger.info("shortest distance: rotating angle: pi / {}".format(shortest_dist_i) )
+logger.info("shortest distance: index of the nearest point to B: {}".format(shortest_dist_index) )
 
 delta_y = k_line_y - By
 delta_x = k_line_x - Bx
@@ -155,13 +131,6 @@ theta = math.pi / shortest_dist_i + math.pi
 phi_new = phi - theta
 new_k_line_x = Bx + abs(delta_x) * np.cos(phi_new)
 new_k_line_y = By + abs(delta_x) * np.sin(phi_new)
-#
-# new_crv = pd.concat(new_k_line_x, new_k_line_y)
-# new_crv.to_csv("result.csv")
-#
-
-
-# print(result_df)
 
 #plot k'ed airfoil boe
 plt.scatter(original_k_line_x, original_k_line_y, marker='x', color='green', alpha=0.7, label='original airfoil curve')
@@ -176,8 +145,8 @@ plt.legend()
 plt.savefig("the picture.pdf")
 
 result_df = pd.concat([new_k_line_x, new_k_line_y], axis =1)
-print(result_df.info)
-result_df.to_csv("newfoilcrv_posn_complete.csv", sep=" ", header=True)
+# print(result_df.info)
+# result_df.to_csv("newfoilcrv_posn_complete.csv", sep=" ", header=True)
 
 
 
@@ -185,16 +154,27 @@ new_k_line_x = new_k_line_x.iloc[0:shortest_dist_index]
 new_k_line_y = new_k_line_y.iloc[0:shortest_dist_index]
 new_k_line_x.iloc[-1] = shortest_dist_x
 new_k_line_y.iloc[-1] = shortest_dist_y
-print(new_k_line_x.size)
-print(new_k_line_y.size)
+# print(new_k_line_x.size)
+# print(new_k_line_y.size)
 result_df = pd.concat([new_k_line_x, new_k_line_y], axis =1)
-print(result_df.info)
-result_df.to_csv("newfoilcrv_posn_medium.csv", sep=" ", header=True)
+logger.debug(result_df.info)
+# result_df.to_csv("newfoilcrv_posn_medium.csv", sep=" ", header=True)
 
 
 new_k_line_x.iloc[-1] = Ax
 new_k_line_y.iloc[-1] = Ay
 result_df = pd.concat([new_k_line_x, new_k_line_y], axis =1)
-print(result_df.info)
-result_df.to_csv("newfoilcrv_posn_last.csv", sep=" ", header=True)
+logger.debug(result_df.info)
+# result_df.to_csv("newfoilcrv_posn_last.csv", sep=" ", header=True)
 
+z_size = new_k_line_x.size
+z_step = Bz - Az
+z_step = z_step / (z_size - 1 )
+new_k_line_z = new_k_line_x
+
+for index in range(z_size):
+    new_k_line_z.iloc[index] = Bz - z_step * index
+
+result_df = pd.concat([new_k_line_x, new_k_line_y, new_k_line_z], axis =1)
+logger.debug(result_df.info)
+result_df.to_csv("newfoilcrv_posn_last_withz.csv", sep=" ", header=True)
